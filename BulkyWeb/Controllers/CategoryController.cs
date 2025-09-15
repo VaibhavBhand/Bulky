3e74d87e3a61d6fb3f57b661bcf1bf5cee208dcb
VaibhavBhand <vaibhavvbhand@gmail.com>
2025-09-15 12:54:38 +0530

    ok

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -6,15 +6,20 @@ namespace BulkyWeb.Controllers
 {
     public class CategoryController : Controller
     {
-       private readonly ApplicationDbContext _db;
-        public CategoryController(ApplicationDbContext db) 
-        { 
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
             _db = db;
         }
         public IActionResult Index()
         {
             List<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
         }
     }
 }
