6e463d85feb9cfcebb6040afacc921f7401a1e6e
VaibhavBhand <vaibhavvbhand@gmail.com>
2025-09-19 15:09:35 +0530

    Razor pages

--- BulkyWebRazor_Temp/Pages/Categories/Create.cshtml.cs
+++ BulkyWebRazor_Temp/Pages/Categories/Create.cshtml.cs
@@ -23,6 +23,7 @@ namespace BulkyWebRazor_Temp.Pages.Categories
         {
             _db.Categories.Add(Category);
             _db.SaveChanges();
+            TempData["Success"] = "Category Created Successfully";
             return RedirectToPage("Index");
         }
     }
--- BulkyWebRazor_Temp/Pages/Categories/Edit.cshtml.cs
+++ BulkyWebRazor_Temp/Pages/Categories/Edit.cshtml.cs
@@ -29,7 +29,7 @@ namespace BulkyWebRazor_Temp.Pages.Categories
             {
                 _db.Categories.Update(Category);
                 _db.SaveChanges();
-                //TempData["success"] = "Category Updated successfully";
+                TempData["success"] = "Category Updated successfully";
 
                 return RedirectToAction("Index");
             }
