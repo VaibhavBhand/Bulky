a4c85e33d2fa6e574c9a2d6a223cdbc2141c6bb5
VaibhavBhand <vaibhavvbhand@gmail.com>
2025-09-27 00:08:35 +0530

    ViewBag ViewData ViewModel

--- Bulky.Models/Product.cs
+++ Bulky.Models/Product.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
@@ -41,9 +42,12 @@ namespace BulkyBook.Models
         [Range(1, 10000)]
         public double Price100 { get; set; }
 
+       
         public int CategoryId { get; set; }
         [ForeignKey("CategoryId")]
+        [ValidateNever]
         public Category? Category { get; set; }
+        [ValidateNever]
         public string ImageUrl { get; set; }
 
     }
